b72dc5a89154cc96be4ac6574fe6d056a4c12178
maksie <dimedmax95@gmail.com>
2024-02-26 21:48:38 +0400

    t_1

---
+++ Desktop/c# education/function/t_1/Program.cs
@@ -0,0 +1,11 @@
+﻿double Calculate_formula(int a, int b, int c, int d)
+{
+
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+
+double result = Calculate_formula(1, 2, 3, 4);
+Console.WriteLine(result);
